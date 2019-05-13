12e6b9d31d8bba0ee5dbd43c7bfa4f2f98d237e9
litianqi <litianqi@yy.com>
2019-05-13 18:30:39 +0800

    test

---
+++ TQTool_test/Classes/NSString+Tool.h
@@ -0,0 +1,21 @@
+//
+//  NSString+Tool.h
+//  GEDU_Demo
+//
+//  Created by Eric on 14/10/29.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSString (Tool)
+
+/**
+ *  得到document路径
+ *
+ *  @return document路径
+ */
++(NSString *)documentPath;
+
+@end
---
+++ TQTool_test/Classes/NSString+Tool.m
@@ -0,0 +1,23 @@
+//
+//  NSString+Tool.m
+//  GEDU_Demo
+//
+//  Created by Eric on 14/10/29.
+//  Copyright (c) 2014年 Eric. All rights reserved.
+//
+
+#import "NSString+Tool.h"
+#import <CommonCrypto/CommonDigest.h>
+#import <sys/xattr.h>
+#import <UIKit/UIKit.h>
+#include <CommonCrypto/CommonHMAC.h>
+
+
+@implementation NSString (Tool)
+
++(NSString *)documentPath {
+    NSLog(@"test");
+    return @"test";
+}
+
+@end
